f0d7462c5d276a718626ece9b11d8151fa2c5626
Amarnath <113051198+AmarnathBheema@users.noreply.github.com>
2022-12-28 13:22:05 +0530

    Update 5 ec2.tf

--- 5 ec2.tf
+++ 5 ec2.tf
@@ -2,7 +2,6 @@
 resource "aws_instance" "demoinstance" { 
  ami = "ami-087c17d1fe0178315" 
  instance_type = "t2.micro" 
- count = 1 
  key_name = "tests" 
  vpc_security_group_ids = ["${aws_security_group.demosg.id}"] 
  subnet_id = "${aws_subnet.public-subnet-1.id}" 
@@ -16,7 +15,6 @@ tags = {
 resource "aws_instance" "demoinstance1" { 
  ami = "ami-087c17d1fe0178315" 
  instance_type = "t2.micro" 
- count = 1 
  key_name = "tests" 
  vpc_security_group_ids = ["${aws_security_group.demosg.id}"] 
  subnet_id = "${aws_subnet.public-subnet-2.id}" 
