d9a70624dc5b64aeb658899219532f12d0923003
Amarnath <113051198+AmarnathBheema@users.noreply.github.com>
2022-12-28 13:17:56 +0530

    Update 8 alb.tf

--- 8 alb.tf
+++ 8 alb.tf
@@ -34,6 +34,6 @@ resource "aws_lb_listener" "external-alb" {
  protocol = "HTTP" 
 default_action { 
  type = "forward" 
- target_group_arn = aws_lb_target_group.external-alb.arn 
+ target_group_arn = aws_lb_target_group.target-elb.arn 
 } 
 }
