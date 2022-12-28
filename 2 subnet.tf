70c6245e556b51c73074c47508e99c388f1c698f
Amarnath <113051198+AmarnathBheema@users.noreply.github.com>
2022-12-28 10:29:35 +0530

    Update 2 subnet.tf

--- 2 subnet.tf
+++ 2 subnet.tf
@@ -1,7 +1,7 @@
 # Creating 1st web subnet 
 resource "aws_subnet" "public-subnet-1" { 
  vpc_id = "${aws_vpc.demovpc.id}"
- cidr_block = "${var.subnet_cidr}" 
+ cidr_block = "${var.subnet1_cidr}" 
  map_public_ip_on_launch = true 
  availability_zone = "us-east-1a" 
 tags = { 
@@ -11,7 +11,7 @@ tags = {
 # Creating 2nd web subnet 
 resource "aws_subnet" "public-subnet-2" { 
  vpc_id = "${aws_vpc.demovpc.id}"
- cidr_block = "${var.subnet1_cidr}" 
+ cidr_block = "${var.subnet2_cidr}" 
  map_public_ip_on_launch = true 
  availability_zone = "us-east-1b" 
 tags = { 
@@ -21,7 +21,7 @@ tags = {
 # Creating 1st application subnet 
 resource "aws_subnet" "application-subnet-1" { 
  vpc_id = "${aws_vpc.demovpc.id}"
- cidr_block = "${var.subnet2_cidr}" 
+ cidr_block = "${var.subnet3_cidr}" 
  map_public_ip_on_launch = false 
  availability_zone = "us-east-1a" 
 tags = { 
@@ -31,7 +31,7 @@ tags = {
 # Creating 2nd application subnet 
 resource "aws_subnet" "application-subnet-2" { 
  vpc_id = "${aws_vpc.demovpc.id}"
- cidr_block = "${var.subnet3_cidr}" 
+ cidr_block = "${var.subnet4_cidr}" 
  map_public_ip_on_launch = false 
  availability_zone = "us-east-1b" 
 tags = { 
@@ -41,7 +41,7 @@ tags = {
 # Create Database Private Subnet 
 resource "aws_subnet" "database-subnet-1" { 
  vpc_id = "${aws_vpc.demovpc.id}" 
- cidr_block = "${var.subnet4_cidr}" 
+ cidr_block = "${var.subnet5_cidr}" 
  availability_zone = "us-east-1a" 
 tags = { 
  Name = "Database Subnet 1" 
@@ -50,7 +50,7 @@ tags = {
 # Create Database Private Subnet 
 resource "aws_subnet" "database-subnet-2" { 
  vpc_id = "${aws_vpc.demovpc.id}" 
- cidr_block = "${var.subnet5_cidr}" 
+ cidr_block = "${var.subnet6_cidr}" 
  availability_zone = "us-east-1a" 
 tags = { 
  Name = "Database Subnet 1" 
