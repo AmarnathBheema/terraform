914bfcda314dd9233211d47e4aa4883f34cdb892
Amarnath <113051198+AmarnathBheema@users.noreply.github.com>
2022-12-28 13:13:40 +0530

    Update 8 alb.tf

--- 8 alb.tf
+++ 8 alb.tf
@@ -13,7 +13,7 @@ resource "aws_lb_target_group" "target-elb" {
  vpc_id = aws_vpc.demovpc.id 
 } 
 resource "aws_lb_target_group_attachment" "attachment" { 
- target_group_arn = aws_lb_target_group.external-alb.arn 
+ target_group_arn = aws_lb_target_group.target-elb.arn 
  target_id = aws_instance.demoinstance.id 
  port = 80 
 depends_on = [ 
@@ -21,7 +21,7 @@ depends_on = [
 ] 
 } 
 resource "aws_lb_target_group_attachment" "attachment1" { 
- target_group_arn = aws_lb_target_group.external-alb.arn 
+ target_group_arn = aws_lb_target_group.target-elb.arn 
  target_id = aws_instance.demoinstance1.id 
  port = 80 
 depends_on = [ 
