17510483f0d9d576c7b58e9ccf6d6c24c1db1320
Amarnath <113051198+AmarnathBheema@users.noreply.github.com>
2022-12-29 17:13:56 +0530

    Update 5 ec2.tf

--- 5 ec2.tf
+++ 5 ec2.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "demoinstance1" {
   instance_type               = "t2.micro"
   key_name                    = "monday"
   vpc_security_group_ids      = ["${aws_security_group.demosg.id}"]
-  subnet_id                   = aws_subnet.public-subnet-2.id
+  subnet_id                   = aws_subnet.application-subnet-1.id
   associate_public_ip_address = true
   user_data                   = file("12 data.sh")
   tags = {
